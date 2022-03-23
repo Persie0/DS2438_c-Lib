fe2526dc25357abb0287564ff62547c619d3f006
Daniel Marek <daroksgame@gmail.com>
2022-03-23 15:19:16 +0100

    refactored to lib

--- Objects/ds2438.d
+++ Objects/ds2438.d
@@ -8,3 +8,4 @@
 .\objects\ds2438.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_armcc.h
 .\objects\ds2438.o: C:\Keil_v5\ARM\PACK\Keil\STM32F1xx_DFP\2.3.0\Device\Include\system_stm32f10x.h
 .\objects\ds2438.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+.\objects\ds2438.o: DS2438_Library.h
